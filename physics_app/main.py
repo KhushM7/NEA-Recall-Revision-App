0276f24b98f77b4b834b51ec078c6947d95a84bd
khushmehta-capacitas <khushmehta@capacitas.co.uk>
2024-07-10 23:17:47 +0100

    Setup

---
+++ physics_app/main.py
@@ -0,0 +1,5 @@
+def type_hints(param: str) -> int:
+    return int(param)
+
+
+print(type_hints("8"))
---
+++ tests/test_main.py
@@ -0,0 +1,3 @@
+from physics_app.main import type_hints
+def test_type_hints():
+    assert type_hints("8")==8
