e8df9a36777b58724311eed20d3993d9504edaa2
khushmehta-capacitas <khushmehta@capacitas.co.uk>
2024-07-16 20:10:50 +0100

    Test TK App

--- physics_app/main.py
+++ physics_app/main.py
@@ -1,1 +1,32 @@
+import tkinter as tk
 
+
+class PhysicsApp(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+        self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        self.message = "Hi there, everyone!"
+        print(self.message)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Physics App")
+    root.state("zoomed")
+
+    app = PhysicsApp(master=root)
+    app.pack(expand=True, fill="both")
+
+    root.mainloop()
